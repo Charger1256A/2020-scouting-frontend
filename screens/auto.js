00411a9d2d8930cb256e425cfa3588ca405bd1a7
Aditya Ram <adityaram@adityas-mbp.lan>
2020-06-17 00:50:31 -0700

    auto modal now opens and closes

--- screens/auto.js
+++ screens/auto.js
@@ -52,56 +52,217 @@ class Auto extends React.Component {
   render() {
     const alliance = this.state.data.alliance;
     const fieldOrientation = this.props.navigation.getParam("fieldOrientation");
-    const pos = powerport[`${alliance}-${fieldOrientation}`];
+    const pos = powerport[fieldOrientation];
     return (
       <View style={{ flex: 1, backgroundColor: '#eaeaea' }}>
         <View style={{ flex: 0.874 }}>
           <View style={autoStyles.MainContainer}>
-          <View style={{flex: 0.7, borderWidth: 4, borderColor: '#d4d4d4'}}>
-            <ImageBackground style={{ flex: 1, resizeMode: 'contain', aspectRatio: 1.33 }} source={fieldImages[fieldOrientation][alliance]}>{
-              
-              pos.map((item, i) => (
-                <TouchableOpacity 
-                  key={i}
-                  style={{position: 'absolute', left: item.left, right: item.right, top: item.top, paddingHorizontal: item.paddingHorizontal, paddingTop: item.paddingTop, paddingBottom: item.paddingBottom, paddingLeft: item.paddingLeft, paddingRight: item.paddingRight}}
-                  // onPress={() => this._toggleModal(item.id, 'cargo')}
+            <View style={{ flex: 0.7, borderWidth: 4, borderColor: '#d4d4d4' }}>
+              <ImageBackground style={{ flex: 1, resizeMode: 'contain', aspectRatio: 1.33 }} source={fieldImages[fieldOrientation][alliance]}>{
+
+                pos.map((item, i) => (
+                  <TouchableOpacity
+                    key={i}
+                    style={{ position: 'absolute', left: item.left, right: item.right, top: item.top, paddingHorizontal: item.paddingHorizontal, paddingTop: item.paddingTop, paddingBottom: item.paddingBottom, paddingLeft: item.paddingLeft, paddingRight: item.paddingRight }}
+                    onPress={() => this._openModal()}
                   >
-                  <Icon name='plus-circle' type='material-community' size={30} color='#34588f' style={autoStyles.Center}/>
-                </TouchableOpacity>
-              ))
-              }              
-            </ImageBackground>
+                    <Icon name='plus-circle' type='material-community' size={30} color='#1cfc03' style={autoStyles.Center} />
+                  </TouchableOpacity>
+                ))
+              }
+              </ImageBackground>
             </View>
-            <View style={{flex: 0.245, marginLeft: 300}}>
-            <ScrollView style={{flex: 0.88}}>
-            <Text style={[{fontWeight: 'bold'}, autoStyles.Font]}>Event Feed</Text>
-            </ScrollView>
-            <View style={{flex: 0.1, marginTop: 10, marginBottom: 50}}>
-            <Text style={autoStyles.Font}>Lower: {this.state.data.lower}{typeof(pos1)}</Text>
-            <Text style={autoStyles.Font}>Outer: {this.state.data.outer}</Text>
-              <Text style={autoStyles.Font}>Inner: {this.state.data.inner}</Text>
+            <View style={{ flex: 0.245, marginLeft: 300 }}>
+              <ScrollView style={{ flex: 0.88 }}>
+                <Text style={[{ fontWeight: 'bold' }, autoStyles.Font]}>Event Feed</Text>
+              </ScrollView>
+              <View style={{ flex: 0.1, marginTop: 10, marginBottom: 50 }}>
+                <Text style={autoStyles.Font}>Lower: {this.state.data.lower}</Text>
+                <Text style={autoStyles.Font}>Outer: {this.state.data.outer}</Text>
+                <Text style={autoStyles.Font}>Inner: {this.state.data.inner}</Text>
               </View>
             </View>
           </View>
         </View>
-        <View style={{flex: 0.126, paddingHorizontal: 40, flexDirection: 'row'}}>
-        <TouchableOpacity style={[autoStyles.UndoButton, {marginHorizontal: 30, marginBottom:25}]}>
-        <View style={autoStyles.Center}>
-                <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Undo</Text>
-              </View>              
-        </TouchableOpacity>
-        <TouchableOpacity style={[prematchStyles.NextButton, {marginHorizontal: 30, marginBottom:25}]}>
-        <View style={autoStyles.Center}>
-                <Blink text='Continue to Teleop'/>
-              </View>              
-        </TouchableOpacity>
+        <View style={{ flex: 0.126, paddingHorizontal: 40, flexDirection: 'row' }}>
+          <TouchableOpacity style={[autoStyles.UndoButton, { marginHorizontal: 30, marginBottom: 25 }]}>
+            <View style={autoStyles.Center}>
+              <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Undo</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={[prematchStyles.NextButton, { marginHorizontal: 30, marginBottom: 25 }]}>
+            <View style={autoStyles.Center}>
+              <Blink text='Continue to Teleop' />
+            </View>
+          </TouchableOpacity>
         </View>
+        <Modal animationInTiming={50} animationIn='fadeIn' animationOutTiming={50} animationOut='fadeOut' style={{ alignItems: 'center' }} isVisible={this.state.isModalVisible}>
+          <View style={autoStyles.ModalContent}>
+            <View style={[autoStyles.Center]}>
+              <View style={{ flex: 1 }}>
+                <Text style={[autoStyles.Font, { textAlign: 'center' }]}>Select Goal</Text>
+              </View>
+            </View>
+            <View style={[autoStyles.Left, { marginBottom: 10 }]}>
+              <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', marginLeft: 20, marginBottom: 50, marginTop: -200 }}>
+                <View style={{ width: 390, height: 50, marginBottom: 10 }}>
+                  <View style={[autoStyles.ScoreButton, { width: '100%' }]} >
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Lower</Text>
+                    </View>
+                  </View>
+                </View>
+                <View style={{ width: 390, height: 50, marginBottom: 10 }}>
+                  <View style={[autoStyles.ScoreButton, { width: '100%' }]}>
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Outer</Text>
+                    </View>
+                  </View>
+                </View>
+                <View style={{ width: 390, height: 50, marginBottom: 10 }}>
+                  <View style={[autoStyles.ScoreButton, { width: '100%' }]}>
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Inner</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+              <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', marginBottom: 50, marginTop: -295, marginLeft: 450 }}>
+                <View style={{ width: 50, height: 50, marginBottom: 10 }}>
+                  <TouchableOpacity style={[autoStyles.AddStackButton, { width: '100%' }]} onPress={() => { this._addLower(1) }}>
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>+</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+                <View style={{ width: 50, height: 50, marginBottom: 10 }}>
+                  <TouchableOpacity style={[autoStyles.AddStackButton, { width: '100%' }]} onPress={() => { this._addOuter(1) }}>
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>+</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+                <View style={{ width: 50, height: 50, marginBottom: 10 }}>
+                  <TouchableOpacity style={[autoStyles.AddStackButton, { width: '100%' }]} onPress={() => { this._addInner(1) }}>
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>+</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', marginBottom: 50, marginTop: -295, marginLeft: 525 }}>
+                <View style={{ width: 50, height: 50, marginBottom: 10 }}>
+                  <TouchableOpacity style={[autoStyles.SubtractStackButton, { width: '100%' }]} onPress={() => { this._addLower(-1) }} >
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>-</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+                <View style={{ width: 50, height: 50, marginBottom: 10 }}>
+                  <TouchableOpacity style={[autoStyles.SubtractStackButton, { width: '100%' }]} onPress={() => { this._addOuter(-1) }} >
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>-</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+                <View style={{ width: 50, height: 50, marginBottom: 10 }}>
+                  <TouchableOpacity style={[autoStyles.SubtractStackButton, { width: '100%' }]} onPress={() => { this._addInner(-1) }} >
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>-</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', marginBottom: 50, marginTop: -295, marginLeft: 600 }}>
+                <View style={{ width: 200, height: 50, marginBottom: 10 }}>
+                  <View style={[autoStyles.ScoreView, { width: '100%' }]} >
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont], { color: '#000000' }}>{this.state.lowerclicks}</Text>
+                    </View>
+                  </View>
+                </View>
+                <View style={{ width: 200, height: 50, marginBottom: 10 }}>
+                  <View style={[autoStyles.ScoreView, { width: '100%' }]}>
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont], { color: '#000000' }}>{this.state.outerclicks}</Text>
+                    </View>
+                  </View>
+                </View>
+                <View style={{ width: 200, height: 50, marginBottom: 10 }}>
+                  <View style={[autoStyles.ScoreView, { width: '100%' }]}>
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont], { color: '#000000' }}>{this.state.innerclicks}</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+              <View style={{ flex: 0.62, flexDirection: 'row', marginBottom: 30 }}>
+                <View style={[autoStyles.Center, { marginHorizontal: 20 }]}>
+                  <TouchableOpacity style={[autoStyles.CancelButton, { width: '100%' }]} onPress={() => this._closemodal()}>
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Cancel</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+                <View style={[autoStyles.Center, { marginHorizontal: 20 }]}>
+                  <TouchableOpacity style={[autoStyles.SaveButton, { width: '100%' }]} onPress={() => this._closemodal()}>
+                    <View style={[autoStyles.Center]}>
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Save</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </View>
+        </Modal>
+
+        <Modal animationInTiming={50} animationIn='fadeIn' animationOutTiming={50} animationOut='fadeOut' style={{ alignItems: 'center' }} isVisible={this.state.control}>
+          <View style={autoStyles.ModalContent}>
+            <View style={[autoStyles.Center, { margin: 10 }]}>
+              <View style={{ flex: 0.2 }}>
+                <Text style={[autoStyles.Font, { textAlign: 'center' }]}>Control Panel</Text>
+              </View>
+              <View style={{ flex: 0.62, flexDirection: 'row', marginBottom: 50, marginHorizontal: 40 }}>
+                <View style={[autoStyles.Center, { marginHorizontal: 20 }]}>
+                  <TouchableOpacity style={[autoStyles.ScoreButton, { width: '100%' }]}>
+                    <View style={[autoStyles.Center, { marginBottom: 20 }]}>
+                      <Image style={{ flex: 1, resizeMode: 'contain', marginTop: 25 }} source={require('../assets/game_pieces/rotation-control.png')} />
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Rotation Control</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+                <View style={[autoStyles.Center, { marginHorizontal: 20 }]}>
+                  <TouchableOpacity style={[autoStyles.ScoreButton, { width: '100%' }]}>
+                    <View style={[autoStyles.Center, { marginBottom: 20 }]}>
+                      <Image style={{ flex: 1, resizeMode: 'contain', marginTop: 25 }} source={require('../assets/game_pieces/position-control.png')} />
+                      <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Position Control</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={{ flex: 0.18, width: '80%' }}>
+                <TouchableOpacity style={autoStyles.CancelButton} onPress={() => this._closemodal()}>
+                  <View style={autoStyles.Center}>
+                    <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Cancel</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
       </View>
 
-    )
+    );
+  }
+  _openModal() {
+    this.setState({ isModalVisible: true })
   }
 
+  _closemodal() {
+    this.setState({ isModalVisible: false })
+  }
 }
+
 autoStyles = StyleSheet.create({
   MainContainer: {
     flex: 1,
