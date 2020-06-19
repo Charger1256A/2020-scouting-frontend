d3886ac2d65590a1156d0f7178f2ad5b2af3cfd1
Aditya Ram <adityaram@adityas-mbp.lan>
2020-06-19 15:03:03 -0700

    stuff

--- screens/auto.js
+++ screens/auto.js
@@ -88,7 +88,8 @@ class Auto extends React.Component {
           </View>
         </View>
         <View style={{ flex: 0.126, paddingHorizontal: 40, flexDirection: 'row' }}>
-          <TouchableOpacity style={[autoStyles.UndoButton, { marginHorizontal: 30, marginBottom: 25 }]} onPress={this._undo()}>
+          {/* <TouchableOpacity style={[autoStyles.UndoButton, { marginHorizontal: 30, marginBottom: 25 }]} onPress={this._undo()}> */}
+          <TouchableOpacity style={[autoStyles.UndoButton, { marginHorizontal: 30, marginBottom: 25 }]}>
             <View style={autoStyles.Center}>
               <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Undo</Text>
             </View>
@@ -270,10 +271,10 @@ class Auto extends React.Component {
 
   _undo() {
     let data = this.state.data;
-    let last = data.autoEvents.pop();
-    if (last != null) {
-      last.event.success == 1 ? data[`auto${this._titleCase(last.event.itemScored)}`] -= 1 : "";
-    }
+    // let last_lower = this.state.data.lower;
+    // let last_outer = this.state.data.outer;
+    // let last_inner = this.state.data.inner;
+    
     this.setState({ data: data });
   }
 }
