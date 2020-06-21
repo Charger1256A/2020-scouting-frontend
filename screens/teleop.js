68534b8357b35c364d996bf1277c284248e3377e
Aditya Ram <adityaram@adityas-mbp.lan>
2020-06-21 12:34:57 -0700

    can open and close control panel modal

--- screens/teleop.js
+++ screens/teleop.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, ImageBackground, Image, ScrollView } from 'react-native';
+import { TouchableOpacity, View, Text, ImageBackground, Image, ScrollView, Alert } from 'react-native';
 import { Icon } from 'react-native-elements';
 import Modal from 'react-native-modal';
 import fieldImages from '../index';
@@ -9,7 +9,7 @@ let postions = require('../assets/button_settings/teleoppositions.json');
 class Teleop extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { data: {}, isControlPanellVisible: false, isPowerCellModalVisible1: false, lower1: 0, outer1: 0, inner1: 0, isPowerCellModalVisible2: false, lower2: 0, outer2: 0, inner2: 0, isPowerCellModalVisible3: false, lower3: 0, outer3: 0, inner3: 0, isPowerCellModalVisible4: false, lower4: 0, outer4: 0, inner4: 0, isPowerCellModalVisible5: false, lower5: 0, outer5: 0, inner5: 0, isPowerCellModalVisible6: false, lower6: 0, outer6: 0, inner6: 0, telelower: 0, teleouter: 0, teleinner: 0 }
+        this.state = { data: {}, isControlPanellVisible: false, isPowerCellModalVisible: false, lower1: 0, outer1: 0, inner1: 0, lower2: 0, outer2: 0, inner2: 0, lower3: 0, outer3: 0, inner3: 0, lower4: 0, outer4: 0, inner4: 0, lower5: 0, outer5: 0, inner5: 0, lower6: 0, outer6: 0, inner6: 0, telelower: 0, teleouter: 0, teleinner: 0 }
     }
 
     componentDidMount() {
@@ -61,7 +61,8 @@ class Teleop extends React.Component {
                             pos.map((item, i) => (
                                 <TouchableOpacity 
                                 key={i}
-                                style={{ position: 'absolute', left: item.left, right: item.right, top: item.top, paddingHorizontal: item.paddingHorizontal, paddingTop: item.paddingTop, paddingBottom: item.paddingBottom, paddingLeft: item.paddingLeft, paddingRight: item.paddingRight }}>
+                                style={{ position: 'absolute', left: item.left, right: item.right, top: item.top, paddingHorizontal: item.paddingHorizontal, paddingTop: item.paddingTop, paddingBottom: item.paddingBottom, paddingLeft: item.paddingLeft, paddingRight: item.paddingRight }}
+                                onPress={() => this._openModal(item.id)}>
                                      <Icon name='plus-circle' type='material-community' size={30} color='#1cfc03' style={autoStyles.Center} />
                                 </TouchableOpacity>  
                             ))
@@ -92,8 +93,53 @@ class Teleop extends React.Component {
                   </View>              
                 </TouchableOpacity>
                 </View>
+                <Modal animationInTiming={50} animationIn='fadeIn' animationOutTiming={50} animationOut='fadeOut' style={{ alignItems: 'center' }} isVisible={this.state.isControlPanellVisible}>
+                    <View style={autoStyles.ModalContent}>
+                        <View style={[autoStyles.Center, { margin: 10 }]}>
+                            <View style={{ flex: 0.2 }}>
+                                <Text style={[autoStyles.Font, { textAlign: 'center' }]}>Control Panel</Text>
+                            </View>
+                            <View style={{ flex: 0.62, flexDirection: 'row', marginBottom: 50, marginHorizontal: 40 }}>
+                                <View style={[autoStyles.Center, { marginHorizontal: 20 }]}>
+                                    <TouchableOpacity style={[autoStyles.ScoreButton, { width: '100%' }]}>
+                                        <View style={[autoStyles.Center, { marginBottom: 20 }]}>
+                                            <Image style={{ flex: 1, resizeMode: 'contain', marginTop: 25 }} source={require('../assets/game_pieces/rotation-control.png')} />
+                                            <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Rotation Control</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
+                                <View style={[autoStyles.Center, { marginHorizontal: 20 }]}>
+                                    <TouchableOpacity style={[autoStyles.ScoreButton, { width: '100%' }]}>
+                                        <View style={[autoStyles.Center, { marginBottom: 20 }]}>
+                                            <Image style={{ flex: 1, resizeMode: 'contain', marginTop: 25 }} source={require('../assets/game_pieces/position-control.png')} />
+                                            <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Position Control</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                            <View style={{ flex: 0.18, width: '80%' }}>
+                                <TouchableOpacity style={autoStyles.CancelButton} onPress={() => this._closeModal()}>
+                                    <View style={autoStyles.Center}>
+                                        <Text style={[prematchStyles.Font, prematchStyles.ButtonFont]}>Cancel</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
             </View>
-        )
+        );
+    }
+    _openModal(id) {
+        if (id == "CP"){
+            this._openCPModal()
+        }
+    }
+    _openCPModal(){
+        this.setState({ isControlPanellVisible: true })
+    }
+    _closeModal() {
+        this.setState({ isControlPanellVisible: false, isPowerCellModalVisible: false})
     }
 }
 export default Teleop;
